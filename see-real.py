a72498b604bec2039119bf22bb13d26c0bddad4c
Philip Yoo <philipsdyoo@msn.com>
2017-03-25 12:00:03 -0400

    Updated script

--- see-real.py
+++ see-real.py
@@ -4,18 +4,13 @@ import os
 import oauth2client
 from oauth2client import client, tools
 import base64
-import mimetypes
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.audio import MIMEAudio
-from email.mime.base import MIMEBase
 from apiclient import errors, discovery
 
 # Some code from Google API site and a StackOverflow post which was then tweaked by me
 # https://developers.google.com/gmail/api/quickstart/python
 # https://developers.google.com/gmail/api/guides/sending
-# http://stackoverflow.com/questions/37201250/sending-email-via-gmail-python
 SCOPES = 'https://www.googleapis.com/auth/gmail.send'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'See-real Box'
@@ -36,14 +31,14 @@ def get_credentials():
         print 'Storing credentials to ' + credential_path
     return credentials
 
-def SendMessage(sender, to, subject, msgHtml, file):
+def send_message(sender, to, subject, msgHtml):
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
-    message1 = CreateMessage(sender, to, subject, msgHtml, file)
-    SendMessageInternal(service, "me", message1)
+    message1 = create_message(sender, to, subject, msgHtml)
+    send_message_internal(service, "me", message1)
 
-def SendMessageInternal(service, user_id, message):
+def send_message_internal(service, user_id, message):
     try:
         message = (service.users().messages().send(userId=user_id, body=message).execute())
         print 'Message Id: %s' % message['id']
@@ -51,7 +46,7 @@ def SendMessageInternal(service, user_id, message):
     except errors.HttpError, error:
         print 'An error occurred: %s' % error
 
-def CreateMessage(sender, to, subject, msgHtml, file):
+def create_message(sender, to, subject, msgHtml):
 	msg = MIMEMultipart('alternative')
 	msg['Subject'] = subject
 	msg['From'] = sender
@@ -60,60 +55,28 @@ def CreateMessage(sender, to, subject, msgHtml, file):
 	message = MIMEText(msgHtml, 'html')
 	msg.attach(message)
 
-	content_type, encoding = mimetypes.guess_type(file)
-
-	if content_type is None or encoding is not None:
-		content_type = 'application/octet-stream'
-	main_type, sub_type = content_type.split('/', 1)
-	if main_type == 'text':
-		fp = open(file, 'rb')
-		message = MIMEText(fp.read(), _subtype=sub_type)
-		fp.close()
-	elif main_type == 'image':
-		fp = open(file, 'rb')
-		message = MIMEImage(fp.read(), _subtype=sub_type)
-		fp.close()
-	elif main_type == 'audio':
-		fp = open(file, 'rb')
-		message = MIMEAudio(fp.read(), _subtype=sub_type)
-		fp.close()
-	else:
-		fp = open(file, 'rb')
-		message = MIMEBase(main_type, sub_type)
-		message.set_payload(fp.read())
-		fp.close()
-
-	print file
-	message.add_header('Content-Disposition', 'attachment', filename=file)
-	msg.attach(message)
-
 	return {'raw': base64.urlsafe_b64encode(msg.as_string())}
 
-def send_email(file):
+def send_email():
 	to = "psy226@nyu.edu"
 	sender = "psy226@nyu.edu"
 	subject = "Intrusion Detected from See-real Box"
-	msgHtml = "Intrusion detected!<br/>Please check attachment."
+	msgHtml = "Intrusion detected!<br/>Connect the USB to camera SD card and check video files in E:/VIDEO/DVREC/"
 	print "Sending email..."
-	SendMessage(sender, to, subject, msgHtml, file)
+	send_message(sender, to, subject, msgHtml)
 
-def check_dir(old_dir, new_dir):
-	for f in new_dir:
-		if f not in old_dir:
-			return f
-	return False
+def main():
+	ser = serial.Serial('COM4', 9600, timeout=0.2)
+	ser.flush()
 
-# Main
-ser = serial.Serial('COM4', 9600, timeout=0.2)
-ser.flush()
+	while True:
+		line = ser.readline().rstrip()
+		if line:
+			print line
+		if line == "Motion detected!":
+			send_email()
+			print "Recording video..."
+		if line == "Motion ended!":
+			print "-------------------"
 
-sd_dir = os.listdir("E:/")
-
-while True:
-	line = ser.readline()
-	if line:
-		print line
-		new_file = check_dir(sd_dir, os.listdir("E:/"))
-		if new_file:
-			print "Attaching" + new_file + " to email"
-			send_email("E:/"+new_file)
+main()
